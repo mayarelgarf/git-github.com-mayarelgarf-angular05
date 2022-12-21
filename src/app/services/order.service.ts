9689f8d1d548ba6148758835ccda89ed23a26d4e
mayarelgarf <mayarmahmoud123@gmail.com>
2022-12-21 13:48:59 +0200

    fix bug

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environment/environment.prod';
+import { environment } from 'src/environment/environment';
 import { AuthService } from './auth.service';
 
 
